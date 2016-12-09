dafd2a59a613e4aa4c9259fed4fe6c535df81f4c
Noufal Ibrahim <noufal@nibrahim.net.in>
2016-12-09 16:23:38 +0530

    Make spacing consistent.
    Use the same (multiples of 4 spaces) everywhere in your code. Otherwise,
    it gets messed up very quickly.

--- occur.py
+++ occur.py
@@ -4,36 +4,34 @@ def tables(n,limit):
 
 
 def mult(num):
-     for i in range (1,num+1):
-           for j in range(1,num+1):
-                  print('{:2}'.format(i*j)),
-           print("\n")
-
+    for i in range (1,num+1):
+        for j in range(1,num+1):
+            print('{:2}'.format(i*j)),
+        print("\n")
 
 
 def disp(num):
-     for i in xrange(1,num+1):
-             if (i%15)==0:
-                         print("FizzBizz")
-             elif i%3==0:
-                         print("Fizz")
-             elif i%5==0:
-                         print("Bizz")
-             else:
-                         print(i)
+    for i in xrange(1,num+1):
+        if (i%15)==0:
+            print("FizzBizz")
+        elif i%3==0:
+            print("Fizz")
+        elif i%5==0:
+            print("Bizz")
+        else:
+            print(i)
 
-                         
 
 def occur(p):
     c = 0
     p = p.lower()
     symbol = 'abcdefghijklmnopqrstuvwxyz'
     for key in symbol:
-            if p.count(key) > 0:
-             continue
-            else:
-             c = 1
-             print key,"is missing"   
+        if p.count(key) > 0:
+            continue
+        else:
+            c = 1
+            print key,"is missing"   
     if c==0:
         print "True"
         print " Panagram"
@@ -46,16 +44,16 @@ def change(money):
     for i in [1000,500,100,50,20,10,1]:
         denom = money/i
         if denom > 0:
-         print i,"*",denom,"=",i*denom
-         money = money - (i * denom )
+            print i,"*",denom,"=",i*denom
+            money = money - (i * denom )
 
 
 def freq(strings):
-   d = {}
-   for i in strings:
-       a = strings.count(i)
-       d.update({i: a})
-   print(str(d))
+    d = {}
+    for i in strings:
+        a = strings.count(i)
+        d.update({i: a})
+    print(str(d))
 
 
 if __name__ == '__main__':
