75142ab3a7964d34915db453a20deda1824f1e70
Jubin S Abraham <jubin.abraham@geckolyst.com>
2016-12-16 10:43:30 +0530

    checked whether the guessed element is already entered or not

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 import random
+count = 10
 def select_word(fname = "/usr/share/dict/words"):
     words  = []
     with open(fname) as f:
@@ -7,20 +8,33 @@ def select_word(fname = "/usr/share/dict/words"):
             if i.isalpha() and i.islower() and len(i) > 6:
                 words.append(i)
     return random.choice(words)
+
+def wrong_guess(guess,a):
+    if guess not in wrong_list:
+            if  guess not in a:
+                wrong_list.append(guess)
+                wrong_list.append(",")
+    else:
+        print "enter something different"
+
+    return wrong_list
+
 a = select_word()
-def place_holder(a,guessed):
-    print a
-    message = ' '
-    for i in a:
-        if guessed == i:
-           message = message.strip() + guessed
-        else:
-           message = message.strip() + "#"
-    return message
-count = 0
-while count < 5:
-    guessed = raw_input("enter a char:")
-    hash_message = place_holder(a,guessed)
-    
-    print hash_message
-    count = count + 1 
+print a
+#print len(a)
+wrong_list =[]
+hash_list = []
+for i in range(len(a)):
+     hash_list.append("#")
+print hash_list
+while count > 0:
+    guessed = raw_input("enter your guess:")
+    wrong_guess_list = wrong_guess(guessed,a)
+    print "Wrong Guesses:",''.join(wrong_guess_list)
+    print "Turns remaining:",count
+    indices = [i for i, x in enumerate(a) if x == guessed]
+    for i in range(len(indices)):
+        pos = indices[i]
+        hash_list[pos] = guessed
+    print ''.join(hash_list)
+    count = count - 1
